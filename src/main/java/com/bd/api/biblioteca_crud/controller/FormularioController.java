e4f04a21dcc04b28f11962c4477974d046f6ed20
Arthur <art.jacintho14@gmail.com>
2025-11-07 15:45:42 -0300

    implementacao thymeleaf

---
+++ src/main/java/com/bd/api/biblioteca_crud/controller/FormularioController.java
@@ -0,0 +1,17 @@
+package com.bd.api.biblioteca_crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping
+public class FormularioController {
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+}
