9ff9cfd049694b4788aa01c2a70233cb74acaa88
arthurolivv <art.jacintho13@gmail.com>
2025-11-24 23:47:35 -0300

    rodape direitos

--- src/main/java/com/bd/api/biblioteca_crud/presentation/mvc/EmprestimoController.java
+++ src/main/java/com/bd/api/biblioteca_crud/presentation/mvc/EmprestimoController.java
@@ -14,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
